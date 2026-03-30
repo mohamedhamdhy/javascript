a3bfae3d2b8b48f762b8d0e13f66c9f66a0cf20f
Mohamed Al Hamdhy <145655248+mohamedhamdhy@users.noreply.github.com>
2026-03-30 09:35:29 +0400

    Basics

---
+++ 02. Debugging/script.js
@@ -0,0 +1,15 @@
+console.log("Welcome To Mohamed Al Hamdhy");
+console.log(123456);
+console.log(18.25);
+console.log(true);
+console.log([58, 78, 96, 35]);
+console.log({ fname: "Joes", age: 25 });
+console.table({ fname: "Joes", age: 25 });
+console.error("Custom Sample Error");
+console.warn("Custom Sample Warning");
+console.time("Timer");
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
+console.timeEnd("Timer");
+// console.clear();
